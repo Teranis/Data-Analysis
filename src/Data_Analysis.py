df5ef82f9fcd633876a3869692d1fc709b75e607
Teranis <timon@rapid-systems.de>
2023-05-31 00:26:03 +0200

    feat: Added exp and lin fitting to doublingtime

--- src/Data_Analysis.py
+++ src/Data_Analysis.py
@@ -1,4 +1,4 @@
-from math import e
+from math import e, log
 import os
 
 import pandas as pd
@@ -16,6 +16,8 @@ import json
 import math
 
 from scipy.optimize import curve_fit
+
+import matplotlib.cm as mcolors
 
 #Organization (use search with the appropriate number of # followed by a space)
 ##### Configs (Outsourced to config.json))
@@ -60,11 +62,12 @@ def importconfigOD():
     total_pos = no_cultures * no_perculture
     OD_norm_data = config['OD_norm_data']
     use_fit = config['OD_use_fit']
+    OD_exp_fit = config['OD_exp_fit']
     for file_name in os.listdir(excel_folder_path):
         if re.search(r"(=?(measure))(=?(.*\.xlsx)$)", file_name):
             excel_path = os.path.join(excel_folder_path, file_name)
     print(excel_path)
-    return excel_path, exp_name, no_timepoints, no_perculture, no_cultures, total_pos, OD_norm_data, use_fit
+    return excel_path, exp_name, no_timepoints, no_perculture, no_cultures, total_pos, OD_norm_data, use_fit, OD_exp_fit
 
 
 ### CC
@@ -152,6 +155,7 @@ def plot_CC(entry, CC_exp_name, CC_path, CC_norm_data):
         ax.set_ylabel('Fraction of cells')
     else:
         ax.set_ylabel('Number of cells')
+    ax.grid(True)
     fig.canvas.manager.set_window_title(CC_exp_name + '_' + name)
     save_path = os.path.join(CC_path, CC_exp_name) + '_' + name + '.png'
     plt.savefig(save_path)
@@ -177,6 +181,7 @@ def plot_together_CC(data, culture_names, CC_norm_data, custom_order, CC_exp_nam
             ax.set_ylabel('Number of cells')
         ax.legend()
         ax = sort_labels_CC(ax, custom_order)
+        ax.grid(True)
         fig.canvas.manager.set_window_title(CC_exp_name + '_' + culture_name)
         save_path = os.path.join(CC_path, CC_exp_name) + '_' + culture_name + '.png'
         plt.savefig(save_path)
@@ -235,19 +240,21 @@ def metadata_legend(data_metadata, total_pos):
     return legend
 
 def fit_curve(time, start_OD, D):
-    ## fitting the data
+    ## exp fit
     OD = start_OD*np.exp(time*D)
     return OD
 
-def fitting(fit_curve, OD, time, start_OD):
-## fitting the data
-    D, cov_ma = curve_fit(fit_curve, time, OD)
-    return D
+def fit_curve_lin(time, start_OD, D):
+    ## linear fit
+    OD = start_OD + time*D
+    return OD
 
-def makingexcl(results, excel_path, exp_name):
-        results = pd.DataFrame(results)
-        results.rename(columns={results.columns[0]: 'Culture', results.columns[1]: 'Hormone conc.', results.columns[2]: 'Doubling time'}, inplace=True)
-        results.to_excel(os.path.join(os.path.dirname(excel_path), exp_name) + '_doublingtime.xlsx', index=False)
+def fitting(fit_curve, OD, time, start_OD, startval):
+## fitting the data
+    D, cov_ma = curve_fit(lambda time, D: fit_curve(time, start_OD, D), time, OD, startval)
+    if OD_exp_fit == True:
+        D = log(2)/D
+    return D
 
 #### Main functions
 #def test():
@@ -257,7 +264,7 @@ def makingexcl(results, excel_path, exp_name):
 ### OD
 def odplot():
     ## creates plots for each culture with normalized OD
-    excel_path, exp_name, no_timepoints, no_perculture, no_cultures, total_pos, OD_norm_data, use_fit = importconfigOD()
+    excel_path, exp_name, no_timepoints, no_perculture, no_cultures, total_pos, OD_norm_data, use_fit, OD_exp_fit = importconfigOD()
     data = import_data_OD(excel_path)
     print(data)
     times = metadata_time(data, no_timepoints)
@@ -274,9 +281,13 @@ def odplot():
             ax.plot(times, data.iloc[j], marker='o', label=legend[j])
         ax.set_title(culturename)
         ax.set_xlabel('Time (h)')
-        ax.set_ylabel('Normalized Optical Density')
+        if OD_norm_data == True:
+            ax.set_ylabel('Normalized Optical Density')
+        else:
+            ax.set_ylabel('Optical Density')
         ax.set_yscale('log')
         ax.legend()
+        ax.grid(True)
         fig.canvas.manager.set_window_title(exp_name + '_' + culturename)
         #print(os.path.join(os.path.dirname(excel_path), exp_name + '_' + culturename))
         plt.savefig(os.path.join(os.path.dirname(excel_path), exp_name + '_' + culturename + '.png'))
@@ -285,37 +296,72 @@ def odplot():
 
 def doublingtime():
     ## calculates doubling time for each culture
-    excel_path, exp_name, no_timepoints, no_perculture, no_cultures, total_pos, OD_norm_data, use_fit = importconfigOD()
+    excel_path, exp_name, no_timepoints, no_perculture, no_cultures, total_pos, OD_norm_data, use_fit, OD_exp_fit = importconfigOD()
     data = import_data_OD(excel_path)
     print(data)
     times = metadata_time(data, no_timepoints)
     names = metadata_names(data, total_pos, no_perculture)
     legend = metadata_legend(data, total_pos)
     data = cut_data(data)
-    print(data)
-    if use_fit != True:
+    print(data) 
+    results = []
+    for i, culturename in enumerate(names):
+        for j in range(i*no_perculture, (i+1)*no_perculture, 1):
+            doubling_times = []
+            for k in range(1, no_timepoints):
+                if data.iloc[j, k] > data.iloc[j, k-1]:
+                    doubling_times.append((np.log(2) * (times[k] - times[k-1]))/ np.log(data.iloc[j, k] / data.iloc[j, k-1]))
+            average = sum(doubling_times) / len(doubling_times)
+            print(culturename, legend[j], average)
+            results.append([culturename, legend[j], average])
+    results = pd.DataFrame(results)
+    results.rename(columns={results.columns[0]: 'Culture', results.columns[1]: 'Hormone conc.', results.columns[2]: 'Doubling time'}, inplace=True)
+    results.to_excel(os.path.join(os.path.dirname(excel_path), exp_name) + '_doublingtime.xlsx', index=False) 
+    if use_fit == True:
+        results_avg = results.copy()
         results = []
-        for i, culturename in enumerate(names):
-            for j in range(i*no_perculture, (i+1)*no_perculture, 1):
-                doubling_times = []
-                for k in range(1, no_timepoints):
-                    if data.iloc[j, k] > data.iloc[j, k-1]:
-                        doubling_times.append((np.log(2) * (times[k] - times[k-1]))/ np.log(data.iloc[j, k] / data.iloc[j, k-1]))
-                average = sum(doubling_times) / len(doubling_times)
-                print(culturename, legend[j], average)
-                results.append([culturename, legend[j], average])
-        makingexcl(results, excel_path, exp_name) 
-    else:
-        results = []
+        if OD_norm_data == True:
+            data = norm_data(data, total_pos, no_timepoints)
         for i, culturename in enumerate(names):
             for j in range(i*no_perculture, (i+1)*no_perculture, 1):
                 ODs = []
-                for k in range(1, no_timepoints):
+                for k in range(no_timepoints):
                     ODs.append(data.iloc[j, k])
-                D = fitting(fit_curve, ODs, np.array(times), data.iloc[j, 1])
-                print(culturename, legend[j], D)
+                if OD_exp_fit == True:
+                    D = fitting(fit_curve, ODs, np.array(times), data.iloc[j, 1], results_avg.iloc[j][2])
+                else:
+                    D = fitting(fit_curve_lin, ODs, np.array(times), data.iloc[j, 1], results_avg.iloc[j][2])
                 results.append([culturename, legend[j], D])
-        makingexcl(results, excel_path, exp_name)
+        results = pd.DataFrame(results)
+        results.rename(columns={results.columns[0]: 'Culture', results.columns[1]: 'Hormone conc.', results.columns[2]: 'Doubling time'}, inplace=True)
+        results.to_excel(os.path.join(os.path.dirname(excel_path), exp_name) + '_doublingtime_fit.xlsx', index=False)
+    color_map = mcolors.get_cmap('tab10')
+    colors = [color_map(i) for i in range(no_perculture)]
+    print(len(colors))
+    for i, culturename in enumerate(names):
+        fig, ax = plt.subplots()
+        for j in range(i*no_perculture, (i+1)*no_perculture, 1):
+            scatter = ax.scatter(times, data.iloc[j], marker='x', label=results.iloc[j][1], color=colors[j-i*no_perculture])
+            y = []
+            for k, _ in enumerate(times):
+                if OD_exp_fit == True:
+                    y.append(fit_curve(times[k], data.iloc[j, 0], log(2)/results.iloc[j][2]))
+                else:
+                    y.append(fit_curve_lin(times[k], data.iloc[j, 0], results.iloc[j][2]))
+            ax.plot(times, y, color=colors[j-i*no_perculture])
+        ax.set_title(culturename)
+        ax.set_xlabel('Time (h)')
+        if OD_norm_data == True:
+            ax.set_ylabel('Normalized Optical Density')
+        else:
+            ax.set_ylabel('Optical Density')
+        ax.set_yscale('log')
+        ax.legend()
+        ax.grid(True)
+        fig.canvas.manager.set_window_title(exp_name + '_' + culturename + '_fit')
+        #print(os.path.join(os.path.dirname(excel_path), exp_name + '_' + culturename))
+        plt.savefig(os.path.join(os.path.dirname(excel_path), exp_name + '_' + culturename + '_fit.png'))
+    plt.show()
 
 
 
